3484a08938072cebd968ca0b405575cf449ffd94
Tora Kozic <tora.kozic@code42.com>
2021-12-03 11:04:31 -0600

    3-2

---
+++ day3/main_part2.py
@@ -0,0 +1,39 @@
+def generate_rates(f):
+    # split numbers by most common digit
+    lines = f.readlines()
+    #l = len(lines[0]) - 1
+    c = helper(lines, 0)
+    o = helper(lines, 0, common=False)
+    p = int(c, 2) * int(o, 2)
+    return p
+
+
+def helper(lines, n, common=True):
+    # split numbers by most common (if common=true) digit in Nth place
+    digit = find_most_common(lines, n)
+    if not common:
+        digit = not digit
+    looking = '1' if digit else '0'
+    # return new list
+    x = []
+    for line in lines:
+        if line[n] == looking:
+            x.append(line)
+    # recurse on new list with n + 1
+    if len(x) == 1:
+        return x[0]
+    return helper(x, n+1, common)
+
+
+def find_most_common(lines, n):
+    a = 0
+    l = len(lines)
+    # could stop count early if by value is determined by halfway point
+    for line in lines:
+        a += int(line[n])
+    return 1 if a >= l/2 else 0
+
+
+f = open('input.txt', 'r')
+power = generate_rates(f)
+print(power)
